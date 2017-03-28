c188f12773084b1f001f9e12fa7af8ee371d3669
TuckyAalto <tucky.aa@gmail.com>
2017-03-28 00:04:46 -0700

    meta site in progress
    meta site in progress

---
+++ meta.js
@@ -0,0 +1,20 @@
+function centerToLeft() {
+    
+    document.getElementById("center_panel").id = "left_panel";
+}
+
+function centerToRight() {
+    
+    document.getElementById("center_panel").id = "right_panel";
+}
+
+function leftToCenter() {
+    
+    document.getElementById("left_panel").id = "center_panel";
+}
+
+function rightToCenter() {
+    
+    document.getElementById("right_panel").id = "center_panel";
+}
+
